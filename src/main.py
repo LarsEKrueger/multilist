8d31c72a8a9d0e5e188bc64dbd1f9851b7b6d242
Lars Krueger <lars_e_krueger@gmx.de>
2026-05-10 10:37:53 +0200

    Prettified UI with jquery-ui

--- src/main.py
+++ src/main.py
@@ -87,6 +87,10 @@ async def jquery():
 async def jquery():
     return serveBinFile("jquery-ui/images/ui-icons_555555_256x240.png", "text/css")
 
+@app.get("/images/ui-icons_777777_256x240.png")
+async def jquery():
+    return serveBinFile("jquery-ui/images/ui-icons_777777_256x240.png", "text/css")
+
 
 @app.get("/version")
 async def version():
